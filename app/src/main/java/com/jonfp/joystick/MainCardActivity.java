6776e9c992e3d742679c8f09b6b6c77296fd1c94
jonfpersson <jonfpersson@gmail.com>
2022-01-24 21:29:54 +0100

    Change color theme

--- app/src/main/java/com/jonfp/joystick/MainCardActivity.java
+++ app/src/main/java/com/jonfp/joystick/MainCardActivity.java
@@ -1,11 +1,17 @@
 package com.jonfp.joystick;
 
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainCardActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.jonfp.joystick.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,4 +19,11 @@ public class MainCardActivity extends AppCompatActivity {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_main_card);
     }
+
+    public void openActivity (View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, "Opening activity");
+        startActivity(intent);
+    }
+
 }
